0f62f6061aa9f1e1dc3452863503bd6eecc2c4db
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-04-30 16:38:18 +0300

    dfa

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,9 +11,9 @@ export default function Home() {
                     <h2 className='font-semibold lg:text-7xl text-3xl leading-11 lg:leading-[94px] dark:text-white'> <span className='text-mred block'>Hello,<span className='dark:text-white text-black'> I'm Mayar Sakr</span></span>   <span className='block'>Ui/Ux Designer</span> based in Egypt</h2>
                     <p className='mt-7 text-gray-600 dark:text-gray-400'>I’m a Creative UI UX Designer with a focus on creating exceptional user interfaces and websites.
                     With specialized training and hands-on experience.</p>
-                    <div className='flex items-center mt-8 justify-center gap-10'>
+                    <div className='flex items-center mt-8 justify-center gap-5 lg:gap-10'>
                         <a className='bt1 flex items-center gap-2  bg-mred py-3 px-5 lg:px-9 rounded-4xl text-white hover:bg-transparent hover:text-mred border-transparent hover:border-mred border duration-300' href="mailto:mayarsakr2000@gmail.com"> <i class='bx bx-envelope' ></i> Email me</a>
-                        <a href="" className='bt2 hover:border-transparent duration-300 flex items-center gap-2 border-mred border text-mred hover:text-white  hover:bg-mred py-3 px-5 lg:px-9 rounded-4xl '>Download CV</a>
+                        <a href="" className='bt2 hover:border-transparent duration-300 flex items-center gap-2 border-mred border text-mred hover:text-white  hover:bg-mred py-3 px-5 lg:px-9 rounded-4xl '>Download CV <i class='bx bx-download text-[20px]'></i></a>
                     </div>
                 </div>
 
