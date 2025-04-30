127f786be35e1111fae6bf48a8d9137bc4def8b3
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-04-30 18:44:02 +0300

    fmdl

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import Navbar from '../Navbar/Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { AnimatePresence } from "framer-motion";
 import Footer from '../Footer/Footer'
 
 export default function Layout() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
     return (
         <>
-
-            <Navbar />
-            <Outlet />
-            <Footer />
-
-
-
-
-
-
+            <Navbar></Navbar>
+            <AnimatePresence mode="wait">
+                <Outlet key={pathname}></Outlet>
+            </AnimatePresence>
+            <Footer></Footer>
 
         </>
     )
