1159fb67fdb8b38a94c6964fa64a8386a22c0293
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-04-30 18:54:14 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Marquee from 'react-fast-marquee'
 
 export default function Footer() {
     return (
         <>
 
-            Footer
+            <div className='bg-black'>
+                <div className=' w-[600px] lg:w-full mx-auto py-6 overflow-hidden'>
+                    <Marquee speed={100}>
+                        <h2 className='text-white '>©Copyright 2025, <span className='text-mred font-bold'>Mayar Sakr</span> All Rights Reserved.</h2>
+                    </Marquee>
+                </div>
+            </div>
 
 
 
